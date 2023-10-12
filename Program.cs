b3c583dacfbe3772811eff0ca35efcd7e665ef92
RafyBejan <146414646+RafyBejan@users.noreply.github.com>
2023-10-12 22:54:03 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            x = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+            rez = x + y;
+            Console.WriteLine(rez);
+        }
+    }
+}
